6031ce6a96117c3a986baf48a236904598e3f19a
nycpivot <mijames@vmware.com>
2020-09-13 19:31:59 -0400

    Removed credentials

--- v1.1.2/install.sh
+++ v1.1.2/install.sh
@@ -25,9 +25,7 @@ sudo mv clusterawsadm-linux-amd64-v0.5.4-vmware-1 /usr/local/bin/clusterawsadm
 chmod +x /usr/local/bin/clusterawsadm
 
 
-export AWS_ACCESS_KEY_ID=AKIA6BLJXPDVJ7WEZEHJ
-export AWS_SECRET_ACCESS_KEY=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
-export AWS_REGION=us-east-1
+
 
 aws cloudformation delete-stack --stack-name cluster-api-provider-aws-sigs-k8s-io
 aws iam delete-user --user-name bootstrapper.cluster-api-provider-aws.sigs.k8s.io
@@ -36,7 +34,3 @@ clusterawsadm alpha bootstrap create-stack
 aws ec2 delete-key-pair --key-name default
 aws ec2 create-key-pair --key-name default --output json | jq .KeyMaterial -r > default.pem
 
-export AWS_CREDENTIALS=$(aws iam create-access-key --user-name bootstrapper.cluster-api-provider-aws.sigs.k8s.io --output json)
-export AWS_ACCESS_KEY_ID=$(echo $AWS_CREDENTIALS | jq .AccessKey.AccessKeyId -r)
-export AWS_SECRET_ACCESS_KEY=$(echo $AWS_CREDENTIALS | jq .AccessKey.SecretAccessKey -r)
-export AWS_B64ENCODED_CREDENTIALS=$(clusterawsadm alpha bootstrap encode-aws-credentials)
